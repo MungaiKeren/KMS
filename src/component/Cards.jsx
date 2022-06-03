dd5775e1d5032e4045594aee62aa6de30190be7d
MungaiKeren <wambukeren@gmail.com>
2022-06-03 23:53:41 +0300

    image edits

--- src/component/Cards.jsx
+++ src/component/Cards.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
-import engine from "../assets/Images/100KVA Carltons-UK(Cummins Engine).jpg"
+import img1 from "../assets/Images/100KVA Carltons-UK(Cummins Engine).jpg"
+import img2 from "../assets/Images/IMG-20220531-WA0009.jpg"
+import img3 from "../assets/Images/SilentDieselGenerator GL1200ED.jpg"
 
 
 const Cards = (props) => {
@@ -8,7 +10,7 @@ const Cards = (props) => {
         <div className="cards">
             <div className="card">
                 <div className="img-top">
-                    <img src={props.img} height="400px" width="400px" alt={props.altText} />
+                    <img src={props.img} height="300px" width="400px" alt={props.altText} />
                 </div>
                 <div className="card-body">
                     <span>
@@ -25,15 +27,15 @@ export function Card(props){
     return (
         <div className="contents">
             <Cards 
-                img={engine}
+                img={img1}
                 cardBody="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
             />
             <Cards 
-                img={engine}
+                img={img2}
                 cardBody="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
             />
             <Cards 
-                img={engine}
+                img={img3}
                 cardBody="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled"
             />
         </div>
