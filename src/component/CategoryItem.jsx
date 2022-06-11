5fd5cf1623d659150af3ed1ca7ef5e842a7f5f43
MungaiKeren <wambukeren@gmail.com>
2022-06-11 11:49:45 +0300

    style category pages

--- src/component/CategoryItem.jsx
+++ src/component/CategoryItem.jsx
@@ -10,12 +10,11 @@ export function CategoryItem(props){
         <BottomNav />
         <hr />
         <div className="breadcrumbs">
-            <span>{props.catName} for sale in Kenya.</span>
-            <p>Home <MdChevronRight /> <span>{props.catName}</span> </p>
+            <span id="span">{props.catName} for sale in Kenya.</span>
+            <p>Home <MdChevronRight size={15} /> <span id="catname">{props.catName}</span> </p>
         </div>
-        <h4 className="text-center">{props.catName}</h4>
         <div className="displayImages">
-            <img src={props.catImage} alt={props.catAltText} />
+            <img id="img" src={props.catImage} alt={props.catAltText} />
         </div>
         <p>{props.catDescription}</p>
         </>
