f25d725d3fe7365a17227718ea61623bcb42aab7
MungaiKeren <wambukeren@gmail.com>
2022-07-09 23:50:52 +0300

    responsive top nav

--- src/component/TopNav.jsx
+++ src/component/TopNav.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import logo from '../assets/Images/logo.png';
+import kms from "../assets/Images/kms.png";
+import kmsLogo from "../assets/Images/kmslogo.png"
 import {MdPhone, MdMailOutline} from "react-icons/md";
 
 export function TopNav(){
@@ -7,14 +9,14 @@ export function TopNav(){
     return (
         <div className="TopNav">
             <div className="left">
-                <img src={logo} alt="logo icon" />
+                <img src={kmsLogo} alt="logo icon" />
             </div>
+            <hr />
             <div className="right">
                 <div className="contact-info">
-                    <h4>
-                        <MdPhone size={20}/> &nbsp;
-                        0706 367 488
-                    </h4>
+                    <span>
+                        <MdPhone size={20} /> &nbsp; 0706 367 488
+                    </span> <br />
                     <MdMailOutline size={20}/> &nbsp;
                    <span className="mailInfo">info@kenyamachineryservices</span>
                 </div>
