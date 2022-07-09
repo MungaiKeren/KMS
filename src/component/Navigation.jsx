b4549244db0ba128e14dda9943f834fe9a017330
MungaiKeren <wambukeren@gmail.com>
2022-07-10 00:57:27 +0300

    minor changes

--- src/component/Navigation.jsx
+++ src/component/Navigation.jsx
@@ -7,18 +7,15 @@ export function Navigation(props){
     return (
         <>
         <div className="navigation">
-            <div className="right">
-                <h4>Kenya Machinery Services</h4>
-            </div>
             <div className="left">
-            <nav className="shift">
-                <ul>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about/">About</a></li>
-                    <li><a href="/services/">Services</a></li>
-                    <li><a href="/contacts/">Contact</a></li>
-                </ul>
-            </nav>
+                <nav className="shift">
+                    <ul>
+                        <li><a href="/">Home</a></li>
+                        <li><a href="/about/">About</a></li>
+                        <li><a href="/services/">Services</a></li>
+                        <li><a href="/contacts/">Contact</a></li>
+                    </ul>
+                </nav>
             </div>
         </div>
         </>
